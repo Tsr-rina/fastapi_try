306028841d5be33720351aa50eb89f6a82675717
rinas <topi2frie3elec@gmail.com>
2022-08-04 14:13:25 +0900

    add art

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from enum import Enum
 from typing import Union
 import json
 
-
-class ModelName(str, Enum):
-    Hon = "Kim"
-    Woo = "Choen"
-
-fake_items_db = [
-    {"item_name": "Apple"},
-    {"item_name": "Banana"},
-    {"item_name": "Lemon"}
-]
-
-
 new_ver = "2"
 
 
@@ -23,48 +11,25 @@ json_load = json.load(json_open)
 
 app = FastAPI()
 
-# 国指定ver
+# country_select_ver
 @app.get("/items/art/{version}/{country}")
 async def read_art_c(version:str, country:str):
-    a = {"Hello_Art":country}
-    if version[-1] == new_ver:
+
+    if country in json_load:
+        global judge
+        judge = True
+    if version[-1] == new_ver and judge==True:
         return json_load[country]
     else:
         if version[-1] != new_ver:
             return "{} is no support".format(version)
         else:
-            return {"X":"Japan"}
+            return "No Page"
+
+# general_select_ver
 @app.get("/items/art/{version}")
 async def read_art_v(version:str):
     if version[-1] == new_ver:
         return json_load
     else:
         return "現在のバージョンはv{}です".format(new_ver)
-
-# @app.get("/items/{item_id}")
-# async def read_items(item_id:str, q:Union[str, None]=None):
-#     if q:
-#         return {"item_id":item_id, "q":q}
-#     return {"item_id":item_id}
-
-@app.get("/items/")
-async def read_item(skip:int=0, limit:int=10):
-    return fake_items_db[skip: skip+limit]
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path":file_path}
-
-@app.get("/name/{model_name}")
-async def read_user_me(model_name:ModelName):
-    if model_name == ModelName.Hon:
-        return {"model_name":model_name, "message":"ATEEZ"}
-
-@app.get("/{item_id}")
-# @app.オペレーション →デコレータ
-# @app.post()
-# @app.put()
-# @app.delete()
-async def read_root(item_id:str):
-    # return {"Hello":"World!"}
-    return {"Hello":item_id}
